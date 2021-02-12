5e921c46d86096d3f58a638a5f31baa52485ddc6
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-02-12 11:28:02 -0300

    fix: convert string do number

--- assets/javascripts/utils.js
+++ assets/javascripts/utils.js
@@ -2,5 +2,12 @@ const Utils = (() => {
 
   return {
     formatNumber: value => Number(value).toLocaleString('pt-BR'),
+
+    convertToNumber: value => {
+      const num = String(value).replace(/\D/g, '');
+      const con = Number(num);
+
+      return isNaN(con) ? 0 : con;
+    }
   }
 })();
--- assets/javascripts/vacination.js
+++ assets/javascripts/vacination.js
@@ -4,12 +4,14 @@ const Vacination = (() => {
     getVaccinatedsByUF: uf => {
       const data = DataStorage.getLocalStorage();
 
-      
       if (uf) {
-        return data.find(state => state.uf.toLowerCase() === uf.toLowerCase()).vacinados;
+        const { vacinados } = data.find(state => state.uf.toLowerCase() === uf.toLowerCase());
+        return Utils.convertToNumber(vacinados);
       }
 
-      return data.map(state => Number(state.vacinados)).reduce((a, b) => a + b, 0);
+      return data
+        .map(state => Utils.convertToNumber(state.vacinados))
+        .reduce((a, b) => a + b, 0);
     } 
   }
 })();
